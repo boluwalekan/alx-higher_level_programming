3806f63a7497c8fb92602ac56eecc23af7bfcbd7
Lekan Boluwade <boluwadelekon@yahoo.com>
2023-01-24 09:28:57 +0000

    more data

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
-
 def uniq_add(my_list=[]):
-    """
-    A function that adds all unique
-    integers in a list (only once for each integer)
-    """
-    new_list = []
-    sum = 0
-    for num in my_list:
-        if num not in new_list:
-            sum += num
-            new_list.append(num)
-    return sum
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_sorted_dictionary(a_dictionary):
-    """
-    A function that prints a dictionary by ordered keys
-    """
-    keys = list(a_dictionary.keys())
-    keys.sort()
-    for key in keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+    for keys in sorted(a_dictionary.keys()):
+        print('{}: {}'. format(keys, a_dictionary[keys]))
